0d7cc347fb60fe3199e37d4839430f0bf290a166
Warren Butt <warren.butt@outlook.com>
2014-07-22 01:52:27 -0600

    Finished TopShelf setup

---
+++ MicroChirper/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace MicroChirper
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            HostFactory.New(x => x.Service<MicroChirpBot>());
+        }
+    }
+
+    class MicroChirpBot : Topshelf.ServiceControl
+    {
+        private readonly System.Timers.Timer _timer;
+        public MicroChirpBot()
+        {
+            _timer = new System.Timers.Timer(1000) {AutoReset = true};
+            _timer.Elapsed += (sender, eventArgs) => NewEmission();
+        }
+
+        private void NewEmission() { throw new NotImplementedException(); }
+
+        public bool Start(HostControl hostControl) { _timer.Start(); return true; }
+        public bool Stop(HostControl hostControl) { _timer.Stop(); return true; }
+    }
+}
