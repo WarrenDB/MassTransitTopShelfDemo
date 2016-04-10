bb87f86d701d840a0cfa56589633e3aff938f7dd
Warren Butt <WarrenDB@users.noreply.github.com>
2016-04-10 00:25:37 -0600

    Updated readme and fixed bug with program.cs file

--- MicroChirper/Program.cs
+++ MicroChirper/Program.cs
@@ -13,7 +13,13 @@ namespace MicroChirper
 
         static void Main()
         {
-            HostFactory.New(x => x.Service<MicroChirpBot>());
+            var host = HostFactory.New(x =>
+            {
+                x.Service<MicroChirpBot>();
+                x.RunAsLocalService();
+            });
+
+            host.Run();
         }
     }
 
@@ -26,7 +32,7 @@ namespace MicroChirper
             _timer.Elapsed += (sender, eventArgs) => NewEmission();
         }
 
-        private void NewEmission() { throw new NotImplementedException(); }
+        private void NewEmission() {Console.WriteLine(String.Format("Chirping at {0}", DateTime.Now.ToString("T")));}
 
         public bool Start(HostControl hostControl) { _timer.Start(); return true; }
         public bool Stop(HostControl hostControl) { _timer.Stop(); return true; }
--- README.md
+++ README.md
@@ -2,5 +2,7 @@ MassTransitTopShelfDemo
 =======================
 
 Demo files for Mass Transit / Top Shelf / Azure Service Bus demo
+_This repository makes use of NuGet package restore functionality. All missing references should be restored upon first build._
 
-Check out the full tutorial coming soon at [my blog](http://warrenbutt.ca/)
+##Tutorial
+###[Part 1 - Developing the service](http://warrenbutt.ca/microservice-architectures-in-net-part-1/)
